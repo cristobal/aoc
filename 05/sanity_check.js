bb5144dc3114fd7cfff0549cfe340205efd25e5c
n24651 <cristobal.dabed@nrk.no>
2020-12-05 20:55:15 +0100

    Add more expressive bit masking

--- 05/sanity_check.js
+++ 05/sanity_check.js
@@ -1,26 +1,36 @@
+// @see https://stackoverflow.com/questions/1436438/how-do-you-set-clear-and-toggle-a-single-bit-in-javascript
+// @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Binary_bitwise_operators
 
-function parseSeatRow (letters) {
-  const values = [
-    letters[0] === 'B' ? 64 : 0,
-    letters[1] === 'B' ? 32 : 0,
-    letters[2] === 'B' ? 16 : 0,
-    letters[3] === 'B' ? 8 : 0,
-    letters[4] === 'B' ? 4 : 0,
-    letters[5] === 'B' ? 2 : 0,
-    letters[6] === 'B' ? 1 : 0
-  ]
+function parseSeatRow (chars) {
+  return 0
+    | (chars[0] === 'B' ? 1 : 0) << 6 // set bit pos 6 = 64
+    | (chars[1] === 'B' ? 1 : 0) << 5 // set bit pos 5 = 32
+    | (chars[2] === 'B' ? 1 : 0) << 4 // set bit pos 4 = 16
+    | (chars[3] === 'B' ? 1 : 0) << 3 // set bit pos 3 =  8
+    | (chars[4] === 'B' ? 1 : 0) << 2 // set bit pos 2 =  4
+    | (chars[5] === 'B' ? 1 : 0) << 1 // set bit pos 1 =  2
+    | (chars[6] === 'B' ? 1 : 0) << 0 // set bit pos 0 =  1
 
-  return values.reduce((acc, value) => value | acc, 0)
+  // can also be expressed as:
+  /*
+  return letters.split('').reverse()
+    .map(char => char === 'B' ? 1 : 0)
+    .reduce((acc, bit, index) => acc | bit << index, 0)
+  */
 }
 
-function parseSeatColumn (letters) {
-  const values = [
-    letters[0] === 'R' ? 4 : 0,
-    letters[1] === 'R' ? 2 : 0,
-    letters[2] === 'R' ? 1 : 0
-  ]
+function parseSeatColumn (chars) {
+  return 0
+    | (chars[0] === 'R' ? 1 : 0) << 2 // set bit pos 2 = 4
+    | (chars[1] === 'R' ? 1 : 0) << 1 // set bit pos 1 = 2
+    | (chars[2] === 'R' ? 1 : 0) << 0 // set bit pos 0 = 1
 
-  return values.reduce((acc, value) => value | acc, 0)
+  // can also be expressed as:
+  /*
+  return letters.split('').reverse()
+    .map(char => char === 'R' ? 1 : 0)
+    .reduce((acc, bit, index) => acc | bit << index, 0)
+  */
 }
 
 function parseSeat (line) {

0af31cd569556bfedd929c98c05c8d85a101f6c0
n24651 <cristobal.dabed@nrk.no>
2020-12-12 01:14:57 +0100

    Day 11 – Part 2

--- 11/seating_system.js
+++ 11/seating_system.js
@@ -1,36 +1,168 @@
 #!/usr/bin/env node
-const grid = require('fs').readFileSync('./input.txt', 'utf-8')
+const board = require('fs').readFileSync('./input.txt', 'utf-8')
   .split('\n')
   .filter(line => line)
   .map(line => line.split(''))
 
-const height = grid.length
-const width = grid[0].length
+const boardHeight = board.length
+const boardWidth = board[0].length
 
-const gridValue =
-  (g, y, x) => {
-    if (y < 0 || y >= height) {
-      return 'X'
+function getBoardValueNW (board, y, x) {
+  let depth = 0
+  for (let dy = y - 1, dx = x - 1; dy >= 0 && dx >= 0; dy--, dx--) {
+    const value = board[dy][dx]
+    if (value === '#' || value === 'L') {
+      return value
     }
+    depth++
 
-    if (x < 0 || x >= width) {
-      return 'X'
+    if (depth >= kernelDepth) {
+      break
     }
-
-    return g[y][x]
   }
+
+  return ''
+}
+
+function getBoardValueN (board, y, x) {
+  let depth = 0
+  for (let dy = y - 1; dy >= 0; dy--) {
+    const value = board[dy][x]
+    if (value === '#' || value === 'L') {
+      return value
+    }
+    depth++
+
+    if (depth >= kernelDepth) {
+      break
+    }
+  }
+
+  return ''
+}
+
+function getBoardValueNE (board, y, x) {
+  let depth = 0
+  for (let dy = y - 1, dx = x + 1; dy >= 0 && dx < boardWidth; dy--, dx++) {
+    const value = board[dy][dx]
+    if (value === '#' || value === 'L') {
+      return value
+    }
+    depth++
+
+    if (depth >= kernelDepth) {
+      break
+    }
+  }
+
+  return ''
+}
+
+function getBoardValueW (board, y, x) {
+  let depth = 0
+  for (let dx = x - 1; dx >= 0; dx--) {
+    const value = board[y][dx]
+    if (value === '#' || value === 'L') {
+      return value
+    }
+    depth++
+
+    if (depth >= kernelDepth) {
+      break
+    }
+  }
+
+  return ''
+}
+
+function getBoardValueE (board, y, x) {
+  let depth = 0
+  for (let dx = x + 1; dx < boardWidth; dx++) {
+    const value = board[y][dx]
+    if (value === '#' || value === 'L') {
+      return value
+    }
+    depth++
+
+    if (depth >= kernelDepth) {
+      break
+    }
+  }
+
+  return ''
+}
+
+function getBoardValueSW (board, y, x) {
+  let depth = 0
+  for (let dy = y + 1, dx = x - 1; dy < boardHeight && dx >= 0; dy++, dx--) {
+    const value = board[dy][dx]
+    if (value === '#' || value === 'L') {
+      return value
+    }
+    depth++
+
+    if (depth >= kernelDepth) {
+      break
+    }
+  }
+
+  return ''
+}
+
+function getBoardValueS (board, y, x) {
+  let depth = 0
+  for (let dy = y + 1; dy < boardHeight; dy++) {
+    const value = board[dy][x]
+    if (value === '#' || value === 'L') {
+      return value
+    }
+    depth++
+
+    if (depth >= kernelDepth) {
+      break
+    }
+  }
+
+  return ''
+}
+
+function getBoardValueSE (board, y, x) {
+  let depth = 0
+  for (let dy = y + 1, dx = x + 1; dy < boardHeight && dx < boardWidth; dy++, dx++) {
+    const value = board[dy][dx]
+    if (value === '#' || value === 'L') {
+      return value
+    }
+    depth++
+
+    if (depth >= kernelDepth) {
+      break
+    }
+  }
+
+  return ''
+}
+
 
 const copyGrid =
   g => [].concat(g.map(r => r.concat()))
 
-let prev = copyGrid(grid)
-let curr = copyGrid(grid)
+let prev = copyGrid(board)
+let curr = copyGrid(board)
+
+// Part 1
+// const adjacentThreshold = 4
+// const kernelDepth = 1
+
+// Part 2
+const adjacentThreshold = 5
+const kernelDepth = Number.MAX_SAFE_INTEGER
 
 while (true) {
   let changes = 0
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
+  for (let y = 0; y < boardHeight; y++) {
+    for (let x = 0; x < boardWidth; x++) {
       const v = prev[y][x]
 
       // floor continue
@@ -39,19 +171,25 @@ while (true) {
       }
 
       let m = [
+        /*
+          NW N NE
+           W * E
+          SW S SE
+        */
+
         // 0, 1, 2
-        gridValue(prev, y - 1, x - 1),
-        gridValue(prev, y - 1, x),
-        gridValue(prev, y - 1, x + 1),
+        getBoardValueNW(prev, y, x), //
+        getBoardValueN(prev, y, x),
+        getBoardValueNE(prev,  y, x),
 
         // 3, 5
-        gridValue(prev, y, x - 1),
-        gridValue(prev, y, x + 1),
+        getBoardValueW(prev, y, x),
+        getBoardValueE(prev, y, x),
 
         // 6, 7, 8
-        gridValue(prev, y + 1, x - 1),
-        gridValue(prev, y + 1, x),
-        gridValue(prev, y + 1, x + 1)
+        getBoardValueSW(prev, y, x),
+        getBoardValueS(prev, y, x),
+        getBoardValueSE(prev, y, x)
       ]
 
       const occupied = m.filter(v => v === '#').length
@@ -65,7 +203,7 @@ while (true) {
 
       // If a seat is occupied (#) and four or more seats adjacent to it are
       // also occupied, the seat becomes empty.
-      if ((v === '#') && (occupied >= 4)) {
+      if ((v === '#') && (occupied >= adjacentThreshold)) {
         curr[y][x] = 'L'
         changes++
         continue
@@ -79,9 +217,5 @@ while (true) {
   }
 }
 
-// for (const row of curr) {
-//   console.log(row.join(''))
-// }
-
-const availableSeats = curr.reduce((acc, row) => acc + row.filter(v => v === '#').length, 0)
-console.log({ availableSeats })
+const seatsTaken = curr.reduce((acc, row) => acc + row.filter(v => v === '#').length, 0)
+console.log({ seatsTaken })

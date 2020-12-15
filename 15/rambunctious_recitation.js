b05e8755e79a5c8932adf947524963388a42d19d
n24651 <cristobal.dabed@nrk.no>
2020-12-15 09:34:52 +0100

    Day 15 - Part 2

--- 15/rambunctious_recitation.js
+++ 15/rambunctious_recitation.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
-// const input = [0, 3, 6]
-const input = [12,1,16,3,11,0]
-const seen = new Map()
-const size = 2020
+function resolveRambunctiousNumber (input, size) {
+  const seen = new Map()
+  input.slice(0, -1)
+    .forEach((value, index) => seen.set(value, index))
 
-input.slice(0, -1)
-  .forEach((value, index) => seen.set(value, index))
+  for (let i = input.length; i < size; i++) {
+    const pi = i - 1
+    const prev = input[pi]
+    const value = seen.has(prev)
+      ? (pi + 1) - (seen.get(prev) + 1)
+      : 0
 
-for (let i = input.length; i < size; i++) {
-  const pi = i - 1
-  const prev = input[pi]
+    input.push(value)
+    seen.set(prev, pi)
+  }
 
-  const value = seen.has(prev)
-    ? (pi + 1) - (seen.get(prev) + 1)
-    : 0
-  input.push(value)
-  seen.set(prev, pi)
+  return input[input.length - 1]
 }
 
-console.log(input[input.length - 1])
+// console.log(`The 2020th number spoken is: ${partOne([0, 3, 6], 2020)}`)
+console.log(`The 2020th number spoken is: ${resolveRambunctiousNumber([12, 1, 16, 3, 11, 0], 2020)}`)
+console.log(`The 30000000th number spoken is: ${resolveRambunctiousNumber([12, 1, 16, 3, 11, 0], 30000000)}`)

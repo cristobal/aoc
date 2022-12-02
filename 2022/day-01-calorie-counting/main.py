7a0bd08bf9ea0adff028b7734ba971bf3c7ab242
Cristobal Dabed <cristobal@dabed.org>
2022-12-02 14:42:56 +0100

    format

--- 2022/day-01-calorie-counting/main.py
+++ 2022/day-01-calorie-counting/main.py
@@ -1,7 +1,7 @@
 def read_values(filename: str) -> list[int]:
     values = []
-    total  = 0
-    for line in open('input.txt').readlines(): 
+    total = 0
+    for line in open(filename).readlines():
         if line == "\n":
             values.append(total)
             total = 0
@@ -9,12 +9,13 @@ def read_values(filename: str) -> list[int]:
             total = total + int(line)
 
     return values
-    
+
+
 def main():
     values = read_values("input.txt")
     print(f"Solution 1: {max(values)}")
     print(f"Solution 2: {sum(sorted(values)[-3:])}")
 
-    
+
 if __name__ == "__main__":
     main()
--- 2022/day-02-rock-paper-scissors/main.py
+++ 2022/day-02-rock-paper-scissors/main.py
@@ -1,62 +1,48 @@
 def read_values(filename: str) -> list[str]:
     values = []
     for line in open(filename).readlines():
-        values.append(
-            line.strip()
-        )
+        values.append(line.strip())
 
     return values
 
+
 def get_solution_lut_solution_one() -> dict[str, int]:
     return {
-        # rock = rock
-        "A X": 1 + 3,
-        # paper > rock
-        "B X": 1 + 0,
-        # scissor < rock
-        "C X": 1 + 6,
-        
-        # rock < paper
-        "A Y": 2 + 6,
-        # paper = paper
-        "B Y": 2 + 3,
-        # scissor > paper
-        "C Y": 2 + 0,
-
-        # rock > scissor
-        "A Z": 3 + 0,
-        # paper < scissor
-        "B Z": 3 + 6,
-        # scissor = scissor
-        "C Z": 3 + 3,    
+        # rock outcomes
+        "A X": 1 + 3,  # rock = rock
+        "B X": 1 + 0,  # paper > rock
+        "C X": 1 + 6,  # scissor < rock
+        # paper outcomes
+        "A Y": 2 + 6,  # rock < paper
+        "B Y": 2 + 3,  # paper = paper
+        "C Y": 2 + 0,  # scissor > paper
+        # scissor outcomes
+        "A Z": 3 + 0,  # rock > scissor
+        "B Z": 3 + 6,  # paper < scissor
+        "C Z": 3 + 3,  # scissor = scissor
     }
+
 
 def get_solution_lut_solution_two() -> dict[str, int]:
     return {
-        # loose => rock > scissor
-        "A X": 3 + 0,
-        # draw  => rock = rock
-        "A Y": 1 + 3,
-        # win   => rock < paper
-        "A Z": 2 + 6,
-
-        # loose => paper > rock
-        "B X": 1 + 0,
-        # draw  => paper = paper
-        "B Y": 2 + 3,
-        # win   => paper < scissor
-        "B Z": 3 + 6,
-
-        # loose => scissor > paper
-        "C X": 2 + 0,
-        # draw  => scissor = scissor
-        "C Y": 3 + 3,
-        # win   => scissor < rock
-        "C Z": 1 + 6,  
+        # choose type based on outcome for rock
+        "A X": 3 + 0,  # loose => rock > scissor
+        "A Y": 1 + 3,  # draw  => rock = rock
+        "A Z": 2 + 6,  # win   => rock < paper
+        # choose type based on outcome for paper
+        "B X": 1 + 0,  # loose => paper > rock
+        "B Y": 2 + 3,  # draw  => paper = paper
+        "B Z": 3 + 6,  # win   => paper < scissor
+        # choose type based on outcome for scissor
+        "C X": 2 + 0,  # loose => scissor > paper
+        "C Y": 3 + 3,  # draw  => scissor = scissor
+        "C Z": 1 + 6,  # win   => scissor < rock
     }
+
 
 def solve_strategy(values: list[str], lut: dict[str, int]) -> int:
     return sum([lut[value] for value in values])
+
 
 def main():
     values = read_values("input.txt")
@@ -64,6 +50,6 @@ def main():
     print(f"Solution 1: {solve_strategy(values, get_solution_lut_solution_one())}")
     print(f"Solution 2: {solve_strategy(values, get_solution_lut_solution_two())}")
 
-    
+
 if __name__ == "__main__":
     main()

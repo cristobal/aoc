86e617fdb8e34bc909528314f8700b42cdeaa6a8
Cristobal Dabed <cristobal@dabed.org>
2022-12-02 13:49:19 +0100

    solution for day 1

---
+++ 2022/day-01-calorie-counting/main.py
@@ -0,0 +1,20 @@
+def read_values(filename: str) -> list[int]:
+    values = []
+    total  = 0
+    for line in open('input.txt').readlines(): 
+        if line == "\n":
+            values.append(total)
+            total = 0
+        else:
+            total = total + int(line)
+
+    return values
+    
+def main():
+    values = read_values("input.txt")
+    print(f"Solution 1: {max(values)}")
+    print(f"Solution 2: {sum(sorted(values)[-3:])}")
+
+    
+if __name__ == "__main__":
+    main()

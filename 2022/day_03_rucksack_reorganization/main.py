058fc0a46620b43b6ac7ebe4c07b7f7c89ad1475
Cristobal Dabed <cristobal@dabed.org>
2022-12-03 09:43:24 +0100

    day 3 - part 02 + refactoring

--- 2022/day_03_rucksack_reorganization/main.py
+++ 2022/day_03_rucksack_reorganization/main.py
@@ -2,39 +2,54 @@ def get_path(filename: str) -> str:
     return __file__.replace("main.py", filename)
 
 
-def read_values(filename: str) -> list[tuple[str, str]]:
+def read_lines(filename: str) -> list[str]:
+    return [line.strip() for line in open(get_path(filename)).readlines()]
+
+
+def get_priority(item: str) -> int:
+    value = ord(item)
+    # a-z
+    if value > 90:
+        return (value - ord("a")) + 1
+
+    # A-Z
+    return (value - ord("A")) + 27
+
+
+def solve_part_1(lines: list[str]) -> int:
     values = []
-    for line in [line.strip() for line in open(get_path(filename)).readlines()]:
+    for line in lines:
         size = int(len(line.strip()) / 2)
         values.append((line[:size], line[-size:]))
 
-    return values
-
-
-def solve_part_1(values: list[tuple[str, str]]) -> int:
     items = []
     for (left, right) in values:
         item = set(left).intersection(set(right)).pop()
         items.append(item)
 
-    priorities = []
-    for item in items:
-        value = ord(item)
-        # a-z
-        if value > 90:
-            priority = (value - ord("a")) + 1
-        # A-Z
-        else:
-            priority = (value - ord("A")) + 27
-        priorities.append(priority)
+    priorities = [get_priority(item) for item in items]
+    return sum(priorities)
 
+
+def solve_part_2(lines: list[str]) -> int:
+    items = []
+    for i in range(0, len(lines), 3):
+        item = (
+            set(lines[i])
+            .intersection(set(lines[i + 1]))
+            .intersection(set(lines[i + 2]))
+            .pop()
+        )
+        items.append(item)
+
+    priorities = [get_priority(item) for item in items]
     return sum(priorities)
 
 
 def main():
-    values = read_values("input.txt")
-    print(f"Solution 1: {solve_part_1(values)}")
-    # print(f"Solution 2: {sum(sorted(values)[-3:])}")
+    lines = read_lines("input.txt")
+    print(f"Solution 1: {solve_part_1(lines)}")
+    print(f"Solution 2: {solve_part_2(lines)}")
 
 
 if __name__ == "__main__":

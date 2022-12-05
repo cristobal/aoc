0b7878817bec2e3c124e7afc21924871aefbbdb8
Cristobal Dabed <cristobal@dabed.org>
2022-12-05 15:15:10 +0100

    day04 - use solver fn approach

--- 2022/day_04_camp_cleanup/main.py
+++ 2022/day_04_camp_cleanup/main.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from collections.abc import Callable
 
 
 def get_set(part) -> set[int]:
@@ -6,44 +7,39 @@ def get_set(part) -> set[int]:
     return set(list(range(int(args[0]), int(args[1]) + 1)))
 
 
-def get_path(filename: str) -> str:
-    return __file__.replace("main.py", filename)
-
-
 def read_values(filename) -> list[tuple[set[int], set[int]]]:
     return [
         (get_set(a), get_set(b))
         for (a, b) in [
-            line.strip().split(",") for line in open(get_path(filename)).readlines()
+            line.strip().split(",")
+            for line in open(__file__.replace("main.py", filename)).readlines()
         ]
     ]
 
 
-def solve_part_1(values: list[tuple[set[int], set[int]]]) -> int:
-    total = 0
-    for (a, b) in values:
-        if a.issubset(b) or b.issubset(a):
-            total = total + 1
+def solver_part_1(total: int, sets: tuple[set[int], set[int]]) -> int:
+    return total + (1 if sets[0].issubset(sets[1]) or sets[1].issubset(sets[0]) else 0)
 
+
+def solver_part_2(total: int, sets: tuple[set[int], set[int]]) -> int:
+    if sets[0].issubset(sets[1]) or sets[1].issubset(sets[0]):
+        return total + 1
+    if len(sets[0].intersection(sets[1])) > 0:
+        return total + 1
     return total
 
 
-def solve_part_2(values: list[tuple[set[int], set[int]]]) -> int:
-    total = 0
-    for (a, b) in values:
-        if a.issubset(b) or b.issubset(a):
-            total = total + 1
-            continue
-        if len(a.intersection(b)) > 0:
-            total = total + 1
-
-    return total
+def solve(
+    solver: Callable[[int, tuple[set[int], set[int]]], int],
+    values: list[tuple[set[int], set[int]]],
+) -> int:
+    return reduce(solver, values, 0)
 
 
 def main():
     values = read_values("input.txt")
-    print(f"Solution 1: {solve_part_1(values)}")
-    print(f"Solution 2: {solve_part_2(values)}")
+    print(f"Solution 1: {solve(solver_part_1, values)}")
+    print(f"Solution 2: {solve(solver_part_2, values)}")
 
 
 if __name__ == "__main__":

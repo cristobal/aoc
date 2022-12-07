efed36f570ee49edf5886189e55c58f3fd9d703d
Cristobal Dabed <cristobal@dabed.org>
2022-12-07 01:16:01 +0100

    Solution in go for bigboy variant 2x speed of python implementation and approx same speed for both solutions.

---
+++ 2022/day_06_tuning_trouble/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+	"time"
+)
+
+func find_marker(datastream []byte, window_size int) int {
+	counter := map[byte]int{}
+
+	for _, char := range "abcdefghijklmnopqrstuvwxyz" {
+		counter[byte(char)] = 0
+	}
+
+	pos := 0
+	total := 0
+	for pos < window_size {
+		char := datastream[pos]
+		counter[char] = counter[char] + 1
+		if counter[char] == 1 {
+			total++
+		}
+		pos++
+	}
+
+	size := binary.Size(datastream)
+	for pos < size {
+		// cleanup
+		old_char := datastream[pos-window_size]
+		counter[old_char] = counter[old_char] - 1
+		if counter[old_char] == 0 {
+			total--
+		}
+
+		char := datastream[pos]
+		counter[char] = counter[char] + 1
+		if counter[char] == 1 {
+			total++
+		}
+
+		if total == window_size {
+			return pos + 1
+		}
+
+		pos++
+	}
+
+	return -1
+}
+
+func main() {
+	/*
+	   url:    https://0x0.st/ods8.txt.7z
+	   bytes:  96M
+	   silver: 85760445
+	   gold:   91845017
+	*/
+	start := time.Now()
+	datastream, _ := os.ReadFile("bigboy.txt")
+	fmt.Printf("Reading file (took: %s)\n", time.Since(start))
+
+	start = time.Now()
+	fmt.Printf("find_marker(datastream, 4) => %v (took: %s)\n", find_marker(datastream, 4), time.Since(start))
+
+	start = time.Now()
+	fmt.Printf("find_marker(datastream, 14) => %v (took: %s)\n", find_marker(datastream, 14), time.Since(start))
+}

302eed8a5c71fb096d588fdae275b9be78aa2e66
Cristobal Dabed <cristobal@dabed.org>
2022-12-07 02:36:41 +0100

    python variant use bytes instead, and drop use of dict

--- 2022/day_06_tuning_trouble/main.py
+++ 2022/day_06_tuning_trouble/main.py
@@ -2,7 +2,7 @@ import time
 import string
 
 
-def find_marker(datastream: str, window_size: int) -> int:
+def find_marker(datastream: bytes, window_size: int) -> int:
     for i in range(len(datastream)):
         if len(set(datastream[i : (i + window_size)])) == window_size:
             return i + window_size
@@ -10,53 +10,55 @@ def find_marker(datastream: str, window_size: int) -> int:
     return -1
 
 
-def find_marker_optimized(datastream: str, window_size: int) -> int:
-    counter = dict()
-    for char in list(string.ascii_lowercase):
-        counter[char] = 0
+def find_marker_optimized(datastream: bytes, window_size: int) -> int:
+    offset = 97
+    counter = [0] * 26
 
     pos = 0
     total = 0
-    for i in range(0, window_size):
-        char = datastream[i]
+    while pos < window_size:
+        char = datastream[pos] - offset
         counter[char] = counter[char] + 1
-        pos = i
         if counter[char] == 1:
             total = total + 1
 
-    for i in range(pos + 1, len(datastream)):
+        pos = pos + 1
+
+    size = len(datastream)
+    while pos < size:
         # cleanup
-        old_char = datastream[i - window_size]
+        old_char = datastream[pos - window_size] - offset
         counter[old_char] = counter[old_char] - 1
         if counter[old_char] == 0:
             total = total - 1
 
-        char = datastream[i]
+        char = datastream[pos] - offset
         counter[char] = counter[char] + 1
         if counter[char] == 1:
             total = total + 1
 
         if total == window_size:
-            return i + 1
+            return pos + 1
 
+        pos = pos + 1
     return -1
 
 
 def main():
     filename = "input.txt"
+    # filename = "bigboy.txt"
     callable = find_marker_optimized if filename.endswith("bigboy.txt") else find_marker
 
-    # filepath = __file__.replace("main.py", "bigboy.txt")
-    filepath = __file__.replace("main.py", "input.txt")
+    filepath = __file__.replace("main.py", filename)
 
     start = time.perf_counter()
-    datastream = open(filepath, "r", encoding="utf-8").readlines()[0]
+    datastream = open(filepath, "rb", buffering=0).read()
     print(
         f"Solution 1: {callable(datastream, 4)} (took: {time.perf_counter() - start})s"
     )
 
     start = time.perf_counter()
-    datastream = open(filepath, "r", encoding="utf-8").readlines()[0]
+    datastream = open(filepath, "rb", buffering=0).read()
     print(
         f"Solution 2: {callable(datastream, 14)} (took: {time.perf_counter() - start})s"
     )
